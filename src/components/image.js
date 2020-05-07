ffc95825cd70eec00f64bf4bc6065426ad89caff
Jarmo Isotalo <jamo@isotalo.fi>
2020-05-07 12:57:39 +0300

    Fix systems-platform config

--- src/components/image.js
+++ src/components/image.js
@@ -19,7 +19,7 @@ const Image = () => (
       query {
         placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
           childImageSharp {
-            fluid(maxWidth: 126) {
+            fluid(maxWidth: 127) {
               ...GatsbyImageSharpFluid
             }
           }
